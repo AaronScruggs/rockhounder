c4645ec85b85da0ee2b54155ab0ce9c6c4679e16
Aaron <aarondscruggs@gmail.com>
2020-08-08 12:26:50 -0700

    Create methods to get and create foreign key ptr values

--- mrds/utilities.py
+++ mrds/utilities.py
@@ -1,6 +1,6 @@
 import csv
 
-from mrds.models import State
+from mrds.models import State, Commodity, OperationType, DevelopmentStatus, WorkType, County
 
 
 class MrdsDataImport:
@@ -18,7 +18,12 @@ class MrdsDataImport:
         'names': 'alt_previous_names',
         'ore_ctrl': 'ore_control'
     }
-    state_map = {}  # {california: 1}
+    state_name_id_map = {}  # ex. {california: 1}
+    county_name_id_map = {}
+    commodity_name_id_map = {}
+    operation_type_name_id_map = {}
+    development_status_name_id_map = {}
+    work_type_name_id_map = {}
 
     def __init__(self, file_name='mrds/data/mrds-f32003.txt'):
         super().__init__()
@@ -26,31 +31,84 @@ class MrdsDataImport:
             d = csv.DictReader(file)
             self.rows = list(d)
 
+        # Dict of functions to get values for foreign key fields.
+        # County is excluded because it is handled separately
         self.fk_func_dict = {
             'state': self.get_state_value,
-            'county': None,
-            'commodity_1': None,
-            'commodity_2': None,
-            'commodity_3': None,
-            'operation_type': None,
-            'development_status': None,
-            'work_type': None,
+            'commodity_1': self.get_commodity_value,
+            'commodity_2': self.get_commodity_value,
+            'commodity_3': self.get_commodity_value,
+            'operation_type': self.get_operation_type_value,
+            'development_status': self.get_development_status_value,
+            'work_type': self.get_work_type_value,
 
         }
         self.fk_field_names = list(self.fk_func_dict.keys())
 
     def insert_data(self):
-        # TODO: For starters only insert rows if they don't exist, don't update existing ones
         pass
 
-    def get_state_value(self, value):
-        obj, is_created = State.objects.get_or_create(name=value)
+    def get_state_value(self, csv_value):
+        existing_database_value = self.state_name_id_map.get(csv_value)
+        if existing_database_value:
+            return existing_database_value
+
+        obj, is_created = State.objects.get_or_create(name=csv_value)
+        self.state_name_id_map[csv_value] = obj.id
+        return obj.id
+
+    def get_county_value(self, county_csv_value, state_csv_value):
+        county_state_key = (county_csv_value, state_csv_value)
+        existing_database_value = self.county_name_id_map.get(county_state_key)
+
+        if existing_database_value:
+            return existing_database_value
+
+        state_id = self.get_state_value(state_csv_value)
+
+        obj, is_created = County.objects.get_or_create(name=county_csv_value, state_id=state_id)
+        self.state_name_id_map[county_state_key] = obj.id
+        return obj.id
+
+    def get_commodity_value(self, csv_value):
+        existing_database_value = self.commodity_name_id_map.get(csv_value)
+        if existing_database_value:
+            return existing_database_value
+
+        obj, is_created = Commodity.objects.get_or_create(name=csv_value)
+        self.commodity_name_id_map[csv_value] = obj.id
+        return obj.id
+
+    def get_operation_type_value(self, csv_value):
+        existing_database_value = self.operation_type_name_id_map.get(csv_value)
+        if existing_database_value:
+            return existing_database_value
+
+        obj, is_created = OperationType.objects.get_or_create(name=csv_value)
+        self.operation_type_name_id_map[csv_value] = obj.id
+        return obj.id
+
+    def get_development_status_value(self, csv_value):
+        existing_database_value = self.development_status_name_id_map.get(csv_value)
+        if existing_database_value:
+            return existing_database_value
+
+        obj, is_created = DevelopmentStatus.objects.get_or_create(name=csv_value)
+        self.development_status_name_id_map[csv_value] = obj.id
+        return obj.id
+
+    def get_work_type_value(self, csv_value):
+        existing_database_value = self.work_type_name_id_map.get(csv_value)
+        if existing_database_value:
+            return existing_database_value
+
+        obj, is_created = WorkType.objects.get_or_create(name=csv_value)
+        self.work_type_name_id_map[csv_value] = obj.id
         return obj.id
 
     def get_fk_ptr_value(self, db_field_name, csv_value):
         # Grab the appropriate func and get/create the ptr value for the given fk field 
-        
-        # TODO: track existing FK objects for all FK fields to avoid extraneous lookups
+
         fk_func = self.fk_func_dict.get(db_field_name)
         if not fk_func:
             return None
@@ -60,17 +118,20 @@ class MrdsDataImport:
 
     def get_row_db_data(self, row):
 
-        # Convert csv field name to database field name
         data = {}
         for key in row.keys():
+
+            # Convert csv field name to database field name
             if key in self.NAME_COLUMN_DIFFS:
                 db_field_name = self.NAME_COLUMN_DIFFS[key]
             else:
                 db_field_name = key
             csv_value = row[key].lower()
-            
-            if db_field_name in self.fk_field_names:
-                # Get or create ptr value for FK field
+
+            if db_field_name == 'county':  # County is special case since it foreign keys to State
+                fk_ptr = self.get_county_value(csv_value, row['state'].lower())
+                data['county_id'] = fk_ptr
+            elif db_field_name in self.fk_field_names:  # Get or create ptr value for FK field
                 fk_ptr = self.get_fk_ptr_value(db_field_name, csv_value)
                 db_field_name_ptr = '{}_id'.format(db_field_name)
                 data[db_field_name_ptr] = fk_ptr
@@ -78,4 +139,3 @@ class MrdsDataImport:
                 data[db_field_name] = csv_value
 
         return data
-
